a5ab61456937f5ab152aaf828f5396650f02343f
rin-windows <phurinmeng@gmail.com>
2025-03-19 17:55:38 -0700

    Add Footer and enhance Projects layout with Card component

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,20 @@
 "use client";
+import Footer from "@/components/Footer";
 import ProjectsFragment from "@/components/ProjectsFragment";
+import { Card, CardContent } from "@/components/ui/card";
 
 export default function Projects() {
-  return <ProjectsFragment />;
+  return (
+    <>
+      <div className="container py-16 mx-auto rounded-none shadow-none border-y-0 border-x"></div>
+      <Card className="relative animate-fade animate-duration-500 py-0 border-none shadow-none gap-0">
+        <div className="border-b w-full border-dashed"></div>
+        <CardContent className="container mx-auto px-0 border-y-0 border-x rounded-none">
+          <ProjectsFragment />
+        </CardContent>
+      </Card>
+
+      <Footer />
+    </>
+  );
 }
