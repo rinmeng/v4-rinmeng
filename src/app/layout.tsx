d1790abb3b2da8b59fe1ceaa3fc4a8873989f123
rin-windows <phurinmeng@gmail.com>
2025-11-11 00:59:20 -0800

    update banner image URLs in metadata to use the correct path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
   openGraph: {
     images: [
       {
-        url: "https://rinm.dev/assets/banner.webp",
+        url: "https://rinm.dev/banner.webp",
         width: 1200,
         height: 630,
         alt: "rin meng banner",
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    images: ["https://rinm.dev/assets/banner.webp"],
+    images: ["https://rinm.dev/banner.webp"],
   },
 };
 
